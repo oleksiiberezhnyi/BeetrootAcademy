a0f72676c267bf6f9b9a451cfa093d18155aad40
Oleksii <o.v.ber@me.com>
2020-11-19 11:46:48 +0200

    Homework #8

---
+++ homework8.py
@@ -0,0 +1,37 @@
+# Task 1
+
+def oops():
+    raise KeyError("Помилка!")
+
+
+def calls_oops(dictionary):
+    try:
+        print(dictionary["name"])
+        oops()
+    except:
+        print("Помилка!")
+
+
+sample_dict = dict(
+    Name="Oleksii",
+    Last_Name="Berezhnyi"
+)
+
+calls_oops(sample_dict)
+
+
+# Task 2
+
+def two_numbers():
+    try:
+        a = int(input("a = "))
+        b = int(input("b = "))
+        c = a ** 2 / b
+        return c
+    except (TypeError, ValueError):
+        print("Це не числа!")
+    except ZeroDivisionError:
+        print("Ділення на нуль!")
+
+
+print(two_numbers())
