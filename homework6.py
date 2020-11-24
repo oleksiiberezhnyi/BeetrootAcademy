c361e3dab78e7ee2daf2e893497573a8909975c8
Oleksii <o.v.ber@me.com>
2020-11-24 10:51:34 +0200

    add other homework

---
+++ homework6.py
@@ -0,0 +1,48 @@
+# Task 1
+
+text_string = input("Введіть текст: ")
+
+split_string = text_string.split()
+
+word_list = dict()
+
+for words in split_string:
+    if word_list.get(words) == None:
+        word_list[words] = 1
+    else:
+        word_list[words] += 1
+
+print(word_list)
+
+# Task 2
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+
+def total_price(stock: dict, prices: dict):
+    total = 0
+    for position in stock:
+        if position in stock and position in prices:
+            total = total + stock.get(position) * prices.get(position)
+    return (total)
+
+
+print(f"Загальна вартість товару: {total_price(stock, prices)} у.о.")
+
+# Task 3
+
+squared = [(i, i ** 2) for i in range(11)]
+
+print(squared)
---
+++ homework7.py
@@ -0,0 +1,50 @@
+# Task 1
+
+movie_title = input("Який ваш найулюбленіший фільм? ")
+
+
+def favorite_movie(name: str):
+    print(f"Ваш улюблений фільм – {name}")
+
+
+favorite_movie(movie_title)
+
+
+# Task 2
+
+def make_country(name="", capital=""):
+    print("Створіть свою країну!")
+    countries = dict()
+    i = 1
+    while True:
+        name = input("Введіть назву країни: ")
+        if len(name) != 0:
+            capital = input("Назвіть столицю цієї країни: ")
+            countries.update({f"Country №{i}": {"Name": name, "Capital": capital}})
+            i += 1
+        else:
+            print(f"All created countries:\n{countries}")
+            return False
+
+
+make_country()
+
+
+# Task 3
+
+def make_operation(operand, *args):
+    try:
+        i = 0
+        res_text = ""
+        while i < len(args):
+            if i < len(args) - 1 and str(abs(i)).isnumeric():
+                res_text = res_text + str(args[i]) + operand
+            elif i == len(args) - 1 and str(abs(i)).isnumeric():
+                res_text = res_text + str(args[i])
+            i += 1
+        print(eval(res_text))
+    except:
+        print("Не правильно введені дані!")
+
+
+make_operation("-", 5, 5, -10, -20)
