3b01bad00250da1aabaa8a4dc8d79f46afecd798
Oleksii <o.v.ber@me.com>
2020-12-03 16:13:53 +0200

    homework 13

---
+++ Homework_13/homework13.py
@@ -0,0 +1,51 @@
+import random
+
+
+# Task 1
+
+def test_function():
+    a = 1
+    b = 2.5
+    c = "string"
+
+
+print(test_function.__code__.co_nlocals)
+
+
+# Task 2
+
+def function_outside():
+    def function_inside(pow):
+        res = [(i, i ** pow) for i in range(1, pow ** pow, pow * 2)]
+        return res
+
+    return function_inside
+
+
+print(function_outside()(2))
+
+
+# Task 3
+
+def choose_func(nums: list, func1, func2):
+    negative = 0
+    for i in nums:
+        if i < 0:
+            negative = 1
+    if negative > 0:
+        return func2(nums)
+    else:
+        return func1(nums)
+
+
+def square_nums(nums):
+    return [num ** 2 for num in nums]
+
+
+def remove_negatives(nums):
+    return [num for num in nums if num > 0]
+
+
+nums1 = [1, 2, 3, 4, 5]
+nums2 = [1, -2, 3, -4, 5]
+print(choose_func(nums1, square_nums, remove_negatives))
