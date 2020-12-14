1aad19179c1ea8540b42925c4f3965b19af8de32
Oleksii <o.v.ber@me.com>
2020-12-14 15:40:26 +0200

    homework 17 Task 3 замінив print на return. в test return tuple в main.py додав print

--- N17_Modules_and_standatd_library/wc.py
+++ N17_Modules_and_standatd_library/wc.py
@@ -2,14 +2,13 @@
 
 def count_lines(filename):
     with open(filename, 'r') as file:
-        print(f'Count of lines in {filename}: {len(file.readlines())}')
+        return len(file.readlines())
 
 
 def count_chars(filename):
     with open(filename, 'r') as file:
-        print(f'Count of chars in {filename}: {len(file.read())}')
+        return len(file.read())
 
 
 def test(filename):
-    count_lines(filename)
-    count_chars(filename)
+    return count_lines(filename), count_chars(filename)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from N17_Modules_and_standatd_library.wc import count_lines, count_chars, test
 
-count_lines("homework1.py")
-count_chars("homework2.py")
-test("homework3.py")
+print(count_lines("homework1.py"))
+print(count_chars("homework2.py"))
+print(test("homework4.py"))
