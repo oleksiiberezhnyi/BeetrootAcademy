dfe74875b8726e8923a65968b2c5b986c43b0fe9
Oleksii <o.v.ber@me.com>
2020-12-17 15:27:05 +0200

    Homework 20 Task 3 fixtures

--- Context_Manager/task3.py
+++ Context_Manager/task3.py
@@ -2,7 +2,6 @@ import pytest
 import typing
 
 
-@pytest.fixture
 def user_data(first_name: str,
               last_name: str,
               email: str,
@@ -16,26 +15,29 @@ def user_data(first_name: str,
             ]
 
 
-def test_user_data():
-    assert user_data() == []
-    # a = user_data("Oleksii", "Berezhnyi", "mail@me.com", 380731737373)
-    # assert a == ["O.", "Berezhnyi", "Oleksii Berezhnyi", "mail@me.com", 380731737373]
+@pytest.fixture
+def data_first_name():
+    return "Oleksii"
 
 
-def user_data_v2(first_name: str,
-                 last_name: str,
-                 email: str,
-                 phone: int
-                 ) -> typing.List:
-    return [f'{first_name[0].capitalize()}.',
-            last_name.title(),
-            f'{first_name.title()} {last_name.title()}',
-            email.lower(),
-            phone
-            ]
+@pytest.fixture
+def data_last_name():
+    return "Berezhnyi"
 
 
-def test_user_data_v2():
-    a = user_data_v2("Oleksii", "Berezhnyi", "mail@me.com", 380731737373)
-    assert a == ["O.", "Berezhnyi", "Oleksii Berezhnyi", "mail@me.com",
-                 380731737373]
+@pytest.fixture
+def data_email_name():
+    return "mail@me.com"
+
+
+@pytest.fixture
+def data_phone_name():
+    return 380731737373
+
+
+def test_user_data(data_first_name, data_last_name, data_email_name,
+                   data_phone_name):
+    assert user_data(data_first_name, data_last_name, data_email_name,
+                     data_phone_name) \
+           == ["O.", "Berezhnyi", "Oleksii Berezhnyi", "mail@me.com",
+               380731737373]
