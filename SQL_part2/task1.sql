bec352fc10201e5b3bcc0a3248e2acd2be56eacb
Oleksii <o.v.ber@me.com>
2021-02-02 16:26:43 +0200

    WIP: homework 38. SQL. Part 2

---
+++ SQL_part2/task1.sql
@@ -0,0 +1,12 @@
+SELECT em.first_name, em.last_name, em.department_id, dep.department_name
+FROM employees as 'em', department as 'dep'
+WHERE em.department_id = dep.department_id;
+
+SELECT em.first_name, em.last_name, dep.department_name, loc.city, loc.state_province
+FROM employees as 'em', department as 'dep', locations as 'loc'
+WHERE em.department_id = dep.department_id
+AND dep.location_id = loc.location_id;
+
+SELECT em.first_name, em.last_name, em.department_id, dep.department_name
+FROM employees as 'em', department as 'dep'
+WHERE em.department_id = dep.department_id AND (em.department_id = 80 OR em.department_id = 40);
