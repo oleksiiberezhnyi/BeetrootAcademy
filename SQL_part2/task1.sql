fdb088213c605dc746d5e00f89671f6314e033d2
Oleksii <o.v.ber@me.com>
2021-02-02 20:09:40 +0200

    WIP: Chat_client

--- SQL_part2/task1.sql
+++ SQL_part2/task1.sql
@@ -10,3 +10,11 @@ AND dep.location_id = loc.location_id;
 SELECT em.first_name, em.last_name, em.department_id, dep.department_name
 FROM employees as 'em', department as 'dep'
 WHERE em.department_id = dep.department_id AND (em.department_id = 80 OR em.department_id = 40);
+
+SELECT department_name
+FROM department
+WHERE department_id NOT IN (SELECT department_id FROM employees);
+
+SELECT em.first_name
+FROM employees as 'em', department as 'dep'
+WHERE em.department_id = dep.department_id;
